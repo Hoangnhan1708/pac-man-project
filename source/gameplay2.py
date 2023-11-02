92693567e7032d384f00a1714f6348c7289a209b
Hoangnhan1708 <truongnhan115@gmail.com>
2023-11-02 19:29:30 +0700

    complete level 2

--- source/gameplay2.py
+++ source/gameplay2.py
@@ -5,29 +5,28 @@ def heuristic(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
 # Get neighbors of a given point
-def get_neighbors(matrix, node):
+def get_neighbors(matrix, node): # node = tuple (x,y) => node[0] = x, node[1] = y
     neighbors = []
     for i, j in [(0, 1), (0, -1), (1, 0), (-1, 0)]: # Phải trái trên dưới
-        if 0 <= node[0] + i < len(matrix) and 0 <= node[1] + j < len(matrix[0]) and matrix[node[0] + i][node[1] + j] != 1 : 
-            if 0 <= node[0] + i < len(matrix) and 0 <= node[1] + j < len(matrix[0]) and matrix[node[0] + i][node[1] + j] != 3:
+        if 0 <= node[0] + i < len(matrix) and 0 <= node[1] + j < len(matrix[0]) and matrix[node[0] + i][node[1] + j] != 1 : # check index of node is out of range and not a wall
+            if 0 <= node[0] + i < len(matrix) and 0 <= node[1] + j < len(matrix[0]) and matrix[node[0] + i][node[1] + j] != 3: # treat monster like wall
                 neighbors.append((node[0] + i, node[1] + j))   
-    return neighbors
+    return neighbors # tuple (x,y)
 
-def get_neighbors_monster(matrix, node):
+def get_neighbors_monster(matrix, node): # this function like above but not process monster
     neighbors = []
     for i, j in [(0, 1), (0, -1), (1, 0), (-1, 0)]: # Phải trái trên dưới
-        if 0 <= node[0] + i < len(matrix) and 0 <= node[1] + j < len(matrix[0]) and matrix[node[0] + i][node[1] + j] != 1 : 
+        if 0 <= node[0] + i < len(matrix) and 0 <= node[1] + j < len(matrix[0]) and matrix[node[0] + i][node[1] + j] != 1 : # check index of node is out of range and not a wall
                 neighbors.append((node[0] + i, node[1] + j))   
     return neighbors
 
 
 # A* algorithm
-def astar(matrix, start, goal):
-    open_set = []
+def astar(matrix, start, goal): # start, goal = tuple(x,y)
+    open_set = [] # list use to store traveled location
     heapq.heappush(open_set, (0, start))
-    came_from = {start: None}
-    g_score = {start: 0}
-    pathAlter = []
+    came_from = {start: None} # dictionary store {location : its own parent}
+    g_score = {start: 0} # g(x) : distance from start location to processing location
     while open_set:
         current = heapq.heappop(open_set)[1] 
         if current == goal:
@@ -38,16 +37,16 @@ def astar(matrix, start, goal):
             path.reverse()
             return path
         
-        for neighbor in get_neighbors(matrix, current):
+        for neighbor in get_neighbors(matrix, current): # Xem ở đây sẽ hiểu : https://www.youtube.com/watch?v=G7XnNtF7UEE&t=368s&ab_channel=%C4%90%E1%BB%97Ph%C3%BAcH%E1%BA%A3o
             temp_g = g_score[current] + 1
             if neighbor not in g_score or temp_g < g_score[neighbor]:
                 came_from[neighbor] = current
                 g_score[neighbor] = temp_g
-                f_score = temp_g + heuristic(neighbor, goal)
+                f_score = temp_g + heuristic(neighbor, goal) # f(x) = g(x) + h(x)
                 heapq.heappush(open_set, (f_score, neighbor))
     return None
 
-def astar2monster(matrix, start, goal):
+def astar_monster(matrix, start, goal):
     open_set = []
     heapq.heappush(open_set, (0, start))
     came_from = {start: None}
@@ -75,57 +74,30 @@ def astar2monster(matrix, start, goal):
 
 
 def find_path_to_food(matrix, pacman_position):
-    foodPosition = None
+    food_position = None
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             if matrix[i][j] == 2:
-                foodPosition = (i,j)        
-    if foodPosition is None:
+                food_position = (i,j)        
+    if food_position is None:
         return None
 
-    return astar(matrix, pacman_position, foodPosition)
-
-def find_path_to_nearest_monster(matrix, pacman_position):
-    monster_positions = []
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            if matrix[i][j] == 2:
-                monster_positions.append((i, j))
-    if not monster_positions:
-        return None
-
-
-    nearest_monster = None
-    for monster in monster_positions:
-        path = astar2monster(matrix, pacman_position, monster)
-        print(path)
-        if path is not None:
-            nearest_monster = monster
-            break
-
-            
-    if nearest_monster is None:
-        return None
-
-    return astar2monster(matrix, pacman_position, nearest_monster)
-
-
-
+    return astar(matrix, pacman_position, food_position)
 
 
 # Update Pacman's position based on the path found by A*
 def update_pacman_position(matrix, pacman_position):
     path_to_food = find_path_to_food(matrix, pacman_position)
     food_position = None
-    monster_position = None
+    monster_positions = []
 
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             if matrix[i][j] == 2:
                 food_position = (i, j)
             if matrix[i][j] == 3:
-                monster_position = (i, j)
-
+                monster_positions.append((i,j))
+    
     if path_to_food:
         next_position = path_to_food[1]  # Lấy vị trí thứ hai trong danh sách đường đi
         if matrix[next_position[0]][next_position[1]] == 3:  # Kiểm tra nếu Pacman chạm vào quái vật
@@ -140,15 +112,17 @@ def update_pacman_position(matrix, pacman_position):
                 return False
             return next_position
     else:
-        # Di chuyển Pacman đến vị trí của quái vật
-        path_to_monster = find_path_to_nearest_monster(matrix,monster_position)
+
+        path_to_monster = astar_monster(matrix, pacman_position, food_position) # still the shortest way to food but pacman must collide with monster
         next_position = path_to_monster[1] if path_to_monster else pacman_position
 
         matrix[pacman_position[0]][pacman_position[1]] = 5  # Đánh dấu lại vị trí mà Pacman đã đi qua
-        matrix[next_position[0]][next_position[1]] = 4  # Di chuyển Pacman đến vị trí tiếp theo
-
-        if (next_position[0], next_position[1]) == monster_position:
+        if (next_position[0], next_position[1]) in monster_positions:
             print("Game Over")
             return False
+        matrix[next_position[0]][next_position[1]] = 4  # Di chuyển Pacman đến vị trí tiếp theo
+
         return next_position
+
+    
     
--- source/main.py
+++ source/main.py
@@ -1,48 +1,47 @@
 import pygame
+import numpy as np
 import logic
 import gameplay1 
 import gameplay2 
-
 pygame.init()
 pygame.font.init()
-import numpy as np
 WIDTH = 700 # width of console
 HEIGHT = 750 # height of console
 screen = pygame.display.set_mode([WIDTH,HEIGHT])
 timer = pygame.time.Clock()
 fps = 10
-colorWall = 'blue'
-colorMonster = 'white'
-colorFood = 'red'
-playerImages=[]
-# matrix = logic.extractMatrix('map1.txt')
-matrix = logic.extractMatrix('map2.txt')
+font = pygame.font.Font('source/assets/font/freesansbold.ttf',32)
+color_wall = 'blue'
+color_monster = 'white'
+color_food = 'red'
+matrix = logic.extractMatrix('map1.txt')
+# matrix = logic.extractMatrix('map2.txt')
 
 rows, cols = len(matrix), len(matrix[0])
-widthTile = (WIDTH//cols) # width of each piece
-heightTile = ((HEIGHT - 50) //rows) # height of each piece
+width_tile = (WIDTH//cols) # width of each piece
+height_tile = ((HEIGHT - 50) //rows) # height of each piece
 
-playerLocation = logic.extractLocation('map1.txt')
-playerX = playerLocation[0] 
-playerY = playerLocation[1]
-initalLocation = (playerX,playerY) 
-matrix[playerX][playerY] = 4
+player_location = logic.extractLocation('map1.txt')
+player_x = player_location[0] 
+player_y = player_location[1]
+
+matrix[player_x][player_y] = 4
 direction = 0
 counter = 0
 
-score_value = 0
-font = pygame.font.Font('source/assets/font/freesansbold.ttf',32)
+score_value = 100
 
 
-# Get image of Pacman
+# Get image 
+player_images=[]
 for i in range(1,5):
-    playerImages.append(pygame.transform.scale(pygame.image.load(f'source/assets/player_images/{i}.png'),(20,20)))
+    player_images.append(pygame.transform.scale(pygame.image.load(f'source/assets/player_images/{i}.png'),(20,20)))
 
-monsterImage =pygame.transform.scale(pygame.image.load(f'source/assets/monster_images/blue.png'),(20,20))
+monster_image =pygame.transform.scale(pygame.image.load(f'source/assets/monster_images/blue.png'),(20,20))
 
-foodImage = pygame.transform.scale(pygame.image.load(f'source/assets/food_image/apple.png'),(25,25))
+food_image = pygame.transform.scale(pygame.image.load(f'source/assets/food_image/apple.png'),(25,25))
 
-def showScore(x,y):
+def render_core(x,y):
     score = font.render("Score: " + str(score_value), True, (255,255,255))
     screen.blit(score,(x,y))
 
@@ -51,21 +50,20 @@ def render(matrix):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if matrix[i][j] == 1: # Wall
-                pygame.draw.rect(screen, colorWall, pygame.Rect(j* widthTile + (0.3 * widthTile) , i * heightTile + (0.3 * heightTile) , widthTile, heightTile))
+                pygame.draw.rect(screen, color_wall, pygame.Rect(j* width_tile + (0.3 * width_tile) , i * height_tile + (0.3 * height_tile) , width_tile, height_tile))
                 
             if matrix[i][j] == 2: # Food
-                # pygame.draw.circle(screen,colorFood, (j* widthTile + (0.3 * widthTile), i * heightTile + (0.3 * heightTile)) ,5)
-                screen.blit(foodImage, (j* widthTile + (0.3 * widthTile), i* heightTile + (0.3 * heightTile)))
+                screen.blit(food_image, (j* width_tile + (0.3 * width_tile), i* height_tile + (0.3 * height_tile)))
                 
             
             if matrix[i][j] == 3: # Monster
-                screen.blit(monsterImage, (j* widthTile + (0.3 * widthTile), i* heightTile + (0.3 * heightTile)))
+                screen.blit(monster_image, (j* width_tile + (0.3 * width_tile), i* height_tile + (0.3 * height_tile)))
             
             if matrix[i][j] == 4: # Pacman
-                screen.blit(playerImages[counter // 5], (j* widthTile + (0.3 * widthTile), i* heightTile + (0.3 * heightTile)))
+                screen.blit(player_images[counter // 5], (j* width_tile + (0.3 * width_tile), i* height_tile + (0.3 * height_tile)))
             
             if matrix[i][j] == 5: # Location which pacman gone
-                pygame.draw.circle(screen, 'white', (j* widthTile + (0.7 * widthTile)  , i * heightTile + (0.7 * heightTile) ) , 5)
+                pygame.draw.circle(screen, 'white', (j* width_tile + (0.7 * width_tile)  , i * height_tile + (0.7 * height_tile) ) , 5)
 
 
 
@@ -78,15 +76,18 @@ while run:
         counter = 0
     screen.fill('black')
     
-    if gameplay2.update_pacman_position(matrix, (playerX, playerY)):
-        playerX, playerY = gameplay2.update_pacman_position(matrix, (playerX, playerY))
-        # playerX, playerY = gameplay1.update_pacman_position(matrix, (playerX, playerY))
+    if gameplay2.update_pacman_position(matrix, (player_x, player_y)):
+        # player_x, player_y = gameplay2.update_pacman_position(matrix, (player_x, player_y))
+        player_x, player_y = gameplay1.update_pacman_position(matrix, (player_x, player_y))
+        score_value -= 1
+        if score_value == 0:
+            run = False
     else:
         run = False
         
     
     render(matrix)
-    showScore(300,700)
+    render_core(300,700)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
